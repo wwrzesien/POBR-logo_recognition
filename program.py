13e0c7ca82bb88b1cf15cdba5cac5306f7f9de00
Wojtek <wrzesien.ww@gmail.com>
2020-04-25 23:41:18 +0200

    Setup environment.

---
+++ program.py
@@ -0,0 +1,15 @@
+"""Rozpoznawanie loga firmy FedEx."""
+
+import cv2
+# import numpy as np
+# from matplotlib import pyplot as plt
+
+
+def main() -> int:
+    """Główna funkcja."""
+    print(cv2.__version__)
+    return 0
+
+
+if __name__ == "__main__":
+    main()
